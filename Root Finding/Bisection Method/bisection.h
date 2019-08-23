1d41b6eb6d78bd9ee04969f22342cb11003e8821
HungryBlueDev <hungrybluedev@gmail.com>
2019-08-23 17:13:10 +0530

    Bisection method all files.

---
+++ Root Finding/Bisection Method/bisection.h
@@ -0,0 +1,32 @@
+#ifndef BISECTION_H
+#define BISECTION_H
+
+#include <stddef.h>
+
+/**
+ * Applies the bisection method of root finding to find a zero of the provided
+ * function f.
+ *
+ * NOTE: This function assumes that the initial criteria for a valid interval
+ * has been met. That is, f(a) * f(b) < 0, or the function has opposite signs
+ * at the two bounds. By the Intermediate Value Theorem, it will then be
+ * guaranteed that the function f has at least 1 zero between a and b.
+ *
+ * @param a         The lower bound of the interval to search in.
+ * @param b         The upper bound of the interval to search in.
+ * @param f         The function whose zero is sought.
+ * @param eps       The permissible error in the final result.
+ * @param n         The pointer in which the number of iterations required will be stored.
+ * @param silent    Should intermediate results be hidden?
+ * @return The zero of a function in the given interval when guaranteed it exists.
+ */
+double bisection(
+        double a,
+        double b,
+        const double (*f)(double),
+        double eps,
+        size_t *n,
+        bool silent
+);
+
+#endif
--- Root Finding/Bisection Method/main.c
+++ Root Finding/Bisection Method/main.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include "bisection.h"
+
+double f(double x) {
+    return (x - 1) * (x - 4);
+}
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-err34-c"
+
+void input_bounds(double *a, double *b, double (*f)(double)) {
+    while (true) {
+        printf("Enter the value of a (lower bound) : ");
+        scanf("%lf", a);
+        printf("Enter the value of b (upper bound) : ");
+        scanf("%lf", b);
+        if (f(*a) * f(*b) < 0) {
+            return;
+        }
+        printf("Invalid range provided. Try again.\n\n");
+    }
+}
+
+double input_error() {
+    double eps;
+    while (true) {
+        printf("Enter the tolerance : ");
+        scanf("%lf", &eps);
+        if (eps > 1e-15) {
+            return eps;
+        }
+        if (eps < 0) {
+            printf("Tolerance must be positive. Try again.\n\n");
+        } else {
+            printf("Tolerance value too low. Try again.\n\n");
+        }
+    }
+}
+
+#pragma clang diagnostic pop
 
 int main() {
-    printf("Hello, World!\n");
+    double a, b;
+    size_t n;
+
+    input_bounds(&a, &b, f);
+    double eps = input_error();
+
+    double ans = bisection(a, b, f, eps, &n, false);
+
+    printf("Answer : %.5lf\nIterations : %zu\n", ans, n);
+
     return 0;
 }
