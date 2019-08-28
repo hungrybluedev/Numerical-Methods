07aef5de8758ec940014668edad069cbc7ba9779
HungryBlueDev <hungrybluedev@gmail.com>
2019-08-29 01:12:42 +0530

    Formatted Regula-Falsi code

--- Root Finding/Regula-Falsi Method/regulafalsi.c
+++ Root Finding/Regula-Falsi Method/regulafalsi.c
@@ -1,7 +1,7 @@
+#include "regulafalsi.h"
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <math.h>
-#include "regulafalsi.h"
 
 /**
  * @param a The lower limit of the interval.
@@ -9,56 +9,46 @@
  * @param f The function whose root is sought.
  * @return An estimate for the location of the root.
  */
-static double next_x(
-        double a,
-        double b,
-        const double (*f)(double)
-) {
-    double fa = f(a);
-    double fb = f(b);
-    return (a * fb - b * fa) / (fb - fa);
+static double next_x(double a, double b, const double (*f)(double)) {
+  double fa = f(a);
+  double fb = f(b);
+  return (a * fb - b * fa) / (fb - fa);
 }
 
-double regula_falsi(
-        double a,
-        double b,
-        double (*f)(double),
-        const double eps,
-        size_t *n,
-        const bool silent
-) {
-    *n = 0; // better safe than sorry
-    double fa = f(a);
-    double fb = f(b);
+double regula_falsi(double a, double b, double (*f)(double), const double eps,
+                    size_t *n, const bool silent) {
+  *n = 0; // better safe than sorry
+  double fa = f(a);
+  double fb = f(b);
 
-    // Check corner cases first
-    if (fabs(fa) < eps) {
-        return a;
+  // Check corner cases first
+  if (fabs(fa) < eps) {
+    return a;
+  }
+  if (fabs(fb) < eps) {
+    return b;
+  }
+
+  while (true) {
+    double x = next_x(a, b, f);
+    double fx = f(x);
+    (*n)++; // Increment right after the function call on guessed location
+
+    if (!silent) {
+      printf("%2zu %.5lf %.5lf %.5lf %.5lf\n", *n, a, b, x, fx);
     }
-    if (fabs(fb) < eps) {
-        return b;
+
+    if (fabs(fx) < eps) {
+      return x;
     }
 
-    while (true) {
-        double x = next_x(a, b, f);
-        double fx = f(x);
-        (*n)++; // Increment right after the function call on guessed location
-
-        if (!silent) {
-            printf("%2zu %.5lf %.5lf %.5lf %.5lf\n", *n, a, b, x, fx);
-        }
-
-        if (fabs(fx) < eps) {
-            return x;
-        }
-
-        // Chose the interval in which the root lies (from the IVT)
-        if (fx * fa < 0) {
-            b = x;
-            fb = fx;
-        } else if (fx * fb < 0) {
-            a = x;
-            fa = fx;
-        }
+    // Chose the interval in which the root lies (from the IVT)
+    if (fx * fa < 0) {
+      b = x;
+      fb = fx;
+    } else if (fx * fb < 0) {
+      a = x;
+      fa = fx;
     }
+  }
 }
--- Root Finding/Regula-Falsi Method/regulafalsi.h
+++ Root Finding/Regula-Falsi Method/regulafalsi.h
@@ -1,11 +1,12 @@
 #ifndef REGULA_FALSI_H
 #define REGULA_FALSI_H
 
+#include <stdbool.h>
 #include <stddef.h>
 
 /**
- * Applies the regula-falsi method of root finding to find a zero of the provided
- * function f.
+ * Applies the regula-falsi method of root finding to find a zero of the
+ * provided function f.
  *
  * NOTE: This function assumes that the initial criteria for a valid interval
  * has been met. That is, f(a) * f(b) < 0, or the function has opposite signs
@@ -16,17 +17,13 @@
  * @param b         The upper bound of the interval to search in.
  * @param f         The function whose zero is sought.
  * @param eps       The permissible error in the final result.
- * @param n         The pointer in which the number of iterations required will be stored.
+ * @param n         The pointer in which the number of iterations required will
+ *                  be stored.
  * @param silent    Should intermediate results be hidden?
- * @return The zero of a function in the given interval when guaranteed it exists.
+ * @return The zero of a function in the given interval when guaranteed it
+ * exists.
  */
-double regula_falsi(
-        double a,
-        double b,
-        double (*f)(double),
-        double eps,
-        size_t *n,
-        bool silent
-);
+double regula_falsi(double a, double b, double (*f)(double), double eps,
+                    size_t *n, bool silent);
 
 #endif
