cc9e2f99efe747352ba32940c9d6092198cb910c
HungryBlueDev <hungrybluedev@gmail.com>
2019-08-29 01:05:07 +0530

    Reformatted the code with Clang Formatter

--- Root Finding/Newton-Raphson Method/newtonraphson.c
+++ Root Finding/Newton-Raphson Method/newtonraphson.c
@@ -1,25 +1,21 @@
+#include <math.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
-#include <math.h>
 
-double newton_raphson(
-        double initial,
-        double (*f)(double),
-        double (*f_prime)(double),
-        double eps,
-        size_t *n,
-        bool silent) {
-    *n = 0;
+double newton_raphson(double initial, double (*f)(double),
+                      double (*f_prime)(double), double eps, size_t *n,
+                      bool silent) {
+  *n = 0;
 
-    double x = initial;
-    while (fabs(f(x)) > eps) {
-        x = x - f(x) / f_prime(x);
-        (*n)++;
+  double x = initial;
+  while (fabs(f(x)) > eps) {
+    x = x - f(x) / f_prime(x);
+    (*n)++;
 
-        if (!silent) {
-            printf("%2zu %10.5lf %10.5lf\n", *n, x, f(x));
-        }
+    if (!silent) {
+      printf("%2zu %10.5lf %10.5lf\n", *n, x, f(x));
     }
-    return x;
+  }
+  return x;
 }
--- Root Finding/Newton-Raphson Method/newtonraphson.h
+++ Root Finding/Newton-Raphson Method/newtonraphson.h
@@ -2,30 +2,28 @@
 #define BISECTION_H
 
 #include <stddef.h>
+#include <stdbool.h>
 
 /**
- * Applies the Newton-Raphson method of root finding to find a zero of the provided
- * function f.
+ * Applies the Newton-Raphson method of root finding to find a zero of the
+ * provided function f.
  *
- * NOTE: This function requires both the f and the f_prime (i.e. the first derivative)
- * to be defined in order to work. In case the function is updated, the user should also
- * update the derivative function.
+ * NOTE: This function requires both the f and the f_prime (i.e. the first
+ * derivative) to be defined in order to work. In case the function is updated,
+ * the user should also update the derivative function.
  *
  * @param initial   The starting guess which will be refined in each iteration.
  * @param f         The function whose zero is sought.
  * @param f_prime   The derivative of f.
  * @param eps       The permissible error in the final result.
- * @param n         The pointer in which the number of iterations required will be stored.
+ * @param n         The pointer in which the number of iterations required will
+ * be stored.
  * @param silent    Should intermediate results be hidden?
- * @return The zero of a function in the given interval when guaranteed it exists.
+ * @return The zero of a function in the given interval when guaranteed it
+ * exists.
  */
-double newton_raphson(
-        double initial,
-        double (*f)(double),
-        double (*f_prime)(double),
-        double eps,
-        size_t *n,
-        bool silent
-);
+double newton_raphson(double initial, double (*f)(double),
+                      double (*f_prime)(double), double eps, size_t *n,
+                      bool silent);
 
 #endif
