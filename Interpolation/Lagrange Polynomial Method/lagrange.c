099be18c974c889fa0415d09ac3b7072c0bec2dc
HungryBlueDev <hungrybluedev@gmail.com>
2019-08-31 12:55:56 +0530

    Modified lagrange.c to include documentation and some cleanup.

--- Interpolation/Lagrange Polynomial Method/lagrange.c
+++ Interpolation/Lagrange Polynomial Method/lagrange.c
@@ -1,20 +1,31 @@
 #include "data.h"
 #include <stddef.h>
 
-double L(size_t n, double x, size_t index, Data *data) {
-  double product = 1, x_index = data[index].x;
-  size_t i;
-  for (i = 0; i < n; i++) {
-    if (i == index)
+/**
+ * Calculates the subexpression for the ith location (index) which will
+ * be added to the accumulator to get the full result.
+ *
+ * @param n     The number of points we have to work with.
+ * @param x     The point to interpolate the function at.
+ * @param index Which subexpression to calculate.
+ * @param data  The collection of the n data points.
+ * @return      The partial value of the interpolation.
+ */
+static double L(size_t n, double x, size_t index, Data *data) {
+  double product = 1, xi = data[index].x;
+  size_t j;
+  for (j = 0; j < n; j++) {
+    // Skip the given index.
+    if (j == index)
       continue;
-    product *= (x - data[i].x) / (x_index - data[i].x);
+    double xj = data[j].x;
+    product *= (x - xj) / (xi - xj);
   }
   return product;
 }
 
 double lagrange_interpolate(double x, size_t n, Data *data) {
   double value = 0;
-  double partial = 1;
   size_t i;
   for (i = 0; i < n; i++) {
     value += L(n, x, i, data) * data[i].y;
