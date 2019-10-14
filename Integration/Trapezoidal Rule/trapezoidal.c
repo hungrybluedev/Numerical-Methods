a905a8c10d42b483cb6161f404cc14854e1bddd8
HungryBlueDev <hungrybluedev@gmail.com>
2019-10-15 00:52:08 +0530

    Clang format for Trapezoidal Rule.

--- Integration/Trapezoidal Rule/trapezoidal.c
+++ Integration/Trapezoidal Rule/trapezoidal.c
@@ -1,13 +1,13 @@
 #include "trapezoidal.h"
 
 double integrate(double (*f)(double), double a, double b, size_t n) {
-    double sum = f(a) + f(b);
-    double gap = (b - a) / n;
+  double sum = f(a) + f(b);
+  double gap = (b - a) / n;
 
-    size_t index;
-    for (index = 1; index < n; index++) {
-        sum += 2 * f(a + gap * index);
-    }
+  size_t index;
+  for (index = 1; index < n; index++) {
+    sum += 2 * f(a + gap * index);
+  }
 
-    return gap * sum / 2;
+  return gap * sum / 2;
 }
--- Integration/Trapezoidal Rule/trapezoidal.h
+++ Integration/Trapezoidal Rule/trapezoidal.h
@@ -17,4 +17,4 @@
  */
 double integrate(double (*f)(double), double a, double b, size_t n);
 
-#endif//TRAPEZOIDAL_H
+#endif // TRAPEZOIDAL_H
